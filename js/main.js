1a94309f616354c5c0426b4bb0293561cd6cfacb
GasanovAnton <Kicym1992@gmail.com>
2020-08-19 10:13:21 +0300

    второе задание
    1 переписал код в es6
    2 добавил метод goodsSum для счета суммы товаров
    3 добавил класс и методы для корзины и для элементов корзины

--- js/main.js
+++ js/main.js
@@ -1,4 +1,112 @@
-const products = [
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        //this.allProducts = [];//массив товаров c добавлением фото
+        this._fetchProducts();
+        this.render();//вывод товаров на страницу
+        this.goodsSum();
+    }
+    _fetchProducts() {
+        this.goods = [
+            { id: 1, title: 'Notebook', price: 2000, img: "https://placeimg.com/320/240/grayscale" },
+            { id: 2, title: 'Mouse', price: 20, img: "https://placeimg.com/320/240/nature" },
+            { id: 3, title: 'Keyboard', price: 200, img: "https://placeimg.com/320/240/tech" },
+            { id: 4, title: 'Gamepad', price: 50, img: "https://placeimg.com/320/240/animals" },
+        ];
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const item = new ProductItem(product);
+
+            // this.allProducts.push(item);
+            block.insertAdjacentHTML("beforeend", item.render());
+            //block.innerHTML += item.render();
+        }
+    }
+
+    goodsSum() {
+        let sum = 0;
+        this.goods.forEach(function (item) {
+            sum += item.price
+        });
+        console.log(sum);
+    }
+}
+
+class ProductItem {
+    constructor(product) {
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = product.img;
+    }
+    render() {
+        return `<div class="product-item">
+                    <h3>${this.title}</h3>
+                    <img src="${this.img}" alt="${this.title}" height: 240>
+                    <p>${this.price}</p>
+                    <button class="buy-btn">
+                        <Span class="buy-btn__text">Купить!</Span>
+                    </button>
+                </div>`
+    }
+}
+
+let list = new ProductList();
+
+
+class BasketList {
+    constructor() {
+        this.products = [];
+    }
+
+    productInTheBasket() {
+        this.products = [
+
+        ]
+    }
+
+    renderBasketItem() {
+
+    }
+
+    sumBasketItem() {
+        let sumInBasket = 0;
+        this.products.forEach(function (item) {
+            sum += item.price
+        });
+        return sumInBasket
+    }
+}
+
+class BasketItem {
+    constructor() {
+
+    }
+
+    renderBasketItem() {
+
+    }
+
+    addBasketItem() {
+
+    }
+
+    removeBasketItem() {
+
+    }
+}
+
+
+
+
+
+
+
+/*const products = [
     { id: 1, title: 'Notebook', price: 2000, img: src = "https://placeimg.com/320/240/grayscale" },
     { id: 2, title: 'Mouse', price: 20, img: src = "https://placeimg.com/320/240/nature" },
     { id: 3, title: 'Keyboard', price: 200, img: src = "https://placeimg.com/320/240/tech" },
@@ -23,4 +131,4 @@ const renderPage = list => {
     document.querySelector('.products').innerHTML = productsListNew;
 };
 
-renderPage(products);
+renderPage(products);*/
