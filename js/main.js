d2a4049e06023111d7b5f0b2a75723ac0e6942f8
GasanovAnton <Kicym1992@gmail.com>
2020-08-22 13:39:40 +0300

    Добавил корзину
    Добавил корзину
    кнопка корзины активна,
    в корзину парсятся данные из json документа

--- js/main.js
+++ js/main.js
@@ -1,54 +1,58 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+const btnBasket = document.querySelector(".btn-cart")
+const basket = document.querySelector(".basket")
 class ProductList {
     constructor(container = '.products') {
         this.container = container;
         this.goods = [];
-        //this.allProducts = [];//массив товаров c добавлением фото
-        this._fetchProducts();
+        this.buttons = [];
+        this.allProducts = [];//массив товаров c добавлением фото
+        //this._fetchProducts();
         this.render();//вывод товаров на страницу
-        this.goodsSum();
+        this._getProducts()
+            .then(data => {
+                this.goods = [...data];
+                this.render()
+                this.buttons = document.querySelectorAll('.buy-btn');
+            });
     }
-    _fetchProducts() {
-        this.goods = [
-            { id: 1, title: 'Notebook', price: 2000, img: "https://placeimg.com/320/240/grayscale" },
-            { id: 2, title: 'Mouse', price: 20, img: "https://placeimg.com/320/240/nature" },
-            { id: 3, title: 'Keyboard', price: 200, img: "https://placeimg.com/320/240/tech" },
-            { id: 4, title: 'Gamepad', price: 50, img: "https://placeimg.com/320/240/animals" },
-        ];
+
+    _getProducts() {
+        return fetch(`${API}/catalogData.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
     }
 
     render() {
         const block = document.querySelector(this.container);
         for (let product of this.goods) {
-            const item = new ProductItem(product);
-
-            // this.allProducts.push(item);
-            block.insertAdjacentHTML("beforeend", item.render());
-            //block.innerHTML += item.render();
+            const productObj = new ProductItem(product);
+            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend', productObj.render());
         }
     }
 
     goodsSum() {
-        let sum = 0;
-        this.goods.forEach(function (item) {
-            sum += item.price
-        });
-        console.log(sum);
+        let sum = this.allProducts.reduce((accum, item) => accum += item.price, 0);
+        return sum;
     }
 }
 
 class ProductItem {
-    constructor(product) {
-        this.title = product.title;
-        this.id = product.id;
+    constructor(product, img = 'https://placeimg.com/320/240/tech') {
+        this.title = product.product_name;
+        this.id = product.id_product;
         this.price = product.price;
-        this.img = product.img;
+        this.img = img;
     }
     render() {
         return `<div class="product-item">
                     <h3>${this.title}</h3>
                     <img src="${this.img}" alt="${this.title}" height: 240>
                     <p>${this.price}</p>
-                    <button class="buy-btn">
+                    <button class="buy-btn" data-id="${this.id}">
                         <Span class="buy-btn__text">Купить!</Span>
                     </button>
                 </div>`
@@ -60,45 +64,93 @@ let list = new ProductList();
 
 class BasketList {
     constructor() {
-        this.products = [];
+        this.productInBasket = [];
+        this.amount;
+        this.countGoods;
+        this._getProducts()
+            .then(data => {
+                this.productInBasket = [...data.contents];
+                this.amount = data.amount;
+                this.countGoods = data.countGoods;
+                this.render()
+            });
     }
 
-    productInTheBasket() {
-        this.products = [
-
-        ]
-    }
-
-    renderBasketItem() {
-
-    }
-
-    sumBasketItem() {
-        let sumInBasket = 0;
-        this.products.forEach(function (item) {
-            sum += item.price
-        });
-        return sumInBasket
-    }
-}
-
-class BasketItem {
-    constructor() {
-
-    }
-
-    renderBasketItem() {
-
+    _getProducts() {
+        return fetch(`${API}/getBasket.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
     }
 
     addBasketItem() {
-
+        
     }
 
     removeBasketItem() {
 
     }
+
+    changeBasketItem() {
+
+    }
+    render() {
+        const block = document.querySelector('.basket__table')
+        for (let product of this.productInBasket) {
+            const basketObj = new BasketItem(product);
+            block.insertAdjacentHTML('beforeend', basketObj.render());
+        }
+        block.insertAdjacentHTML('beforeend', this.renderTotal());
+    }
+
+    renderTotal() {
+        return `<tr>
+                    <td class="basket__ID"> </td>
+                    <td class="basket__item-name"> Итог </td>
+                    <td class="basket__price">${this.amount}</td>
+                    <td class="basket__count">${this.countGoods}</td>
+                </tr>`
+    }
 }
+
+class BasketItem {
+    constructor(productInBasket) {
+        this.title = productInBasket.product_name;
+        this.id = productInBasket.id_product;
+        this.price = productInBasket.price;
+        this.count = productInBasket.quantity;
+    }
+    render() {
+        return `<tr>
+                    <td class="basket__ID">${this.id}</td>
+                    <td class="basket__item-name">${this.title}</td>
+                    <td class="basket__price">${this.price}</td>
+                    <td class="basket__count">${this.count}</td>
+                </tr>`
+    }
+}
+
+btnBasket.addEventListener('click', () => {
+    basket.classList.toggle('hidden')
+})
+
+let newBasketList = new BasketList();
+
+
+
+
+
+
+/*_fetchProducts() {
+        this.goods = [
+            { id: 1, title: 'Notebook', price: 2000, img: "https://placeimg.com/320/240/grayscale" },
+            { id: 2, title: 'Mouse', price: 20, img: "https://placeimg.com/320/240/nature" },
+            { id: 3, title: 'Keyboard', price: 200, img: "https://placeimg.com/320/240/tech" },
+            { id: 4, title: 'Gamepad', price: 50, img: "https://placeimg.com/320/240/animals" },
+        ];
+    }*/
+
 
 
 
