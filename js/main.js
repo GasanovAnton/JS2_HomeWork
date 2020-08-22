d11f9c9213846ff96ea1b593e6f0cf5a98b71289
GasanovAnton <Kicym1992@gmail.com>
2020-08-22 15:33:47 +0300

    Думаю дальше
    Думаю дальше
    пробую реализовтаь кнопку купить.
    добавил слушатель событий на кноки купить

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
-const btnBasket = document.querySelector(".btn-cart")
-const basket = document.querySelector(".basket")
+const btnBasket = document.querySelector(".btn-cart");
+const basket = document.querySelector(".basket");
+let buttons = [];
 class ProductList {
     constructor(container = '.products') {
         this.container = container;
         this.goods = [];
-        this.buttons = [];
         this.allProducts = [];//массив товаров c добавлением фото
         //this._fetchProducts();
         this.render();//вывод товаров на страницу
         this._getProducts()
             .then(data => {
                 this.goods = [...data];
-                this.render()
-                this.buttons = document.querySelectorAll('.buy-btn');
+                this.render();
+                this.btnActive();
             });
     }
 
@@ -37,6 +37,15 @@ class ProductList {
     goodsSum() {
         let sum = this.allProducts.reduce((accum, item) => accum += item.price, 0);
         return sum;
+    }
+
+    btnActive() {
+        buttons = document.querySelectorAll('.buy-btn');
+        buttons.forEach(btn => {
+            btn.addEventListener('click', () => {
+                console.log(btn.dataset.id)
+            })
+        })
     }
 }
 
@@ -85,7 +94,7 @@ class BasketList {
     }
 
     addBasketItem() {
-        
+
     }
 
     removeBasketItem() {
@@ -135,52 +144,5 @@ btnBasket.addEventListener('click', () => {
     basket.classList.toggle('hidden')
 })
 
+
 let newBasketList = new BasketList();
-
-
-
-
-
-
-/*_fetchProducts() {
-        this.goods = [
-            { id: 1, title: 'Notebook', price: 2000, img: "https://placeimg.com/320/240/grayscale" },
-            { id: 2, title: 'Mouse', price: 20, img: "https://placeimg.com/320/240/nature" },
-            { id: 3, title: 'Keyboard', price: 200, img: "https://placeimg.com/320/240/tech" },
-            { id: 4, title: 'Gamepad', price: 50, img: "https://placeimg.com/320/240/animals" },
-        ];
-    }*/
-
-
-
-
-
-
-
-
-/*const products = [
-    { id: 1, title: 'Notebook', price: 2000, img: src = "https://placeimg.com/320/240/grayscale" },
-    { id: 2, title: 'Mouse', price: 20, img: src = "https://placeimg.com/320/240/nature" },
-    { id: 3, title: 'Keyboard', price: 200, img: src = "https://placeimg.com/320/240/tech" },
-    { id: 4, title: 'Gamepad', price: 50, img: src = "https://placeimg.com/320/240/animals" },
-];
-
-//Функция для формирования верстки каждого товара
-const renderProduct = (item) => {
-    return `<div class="product-item">
-                <h3>${item.title}</h3>
-                <img src="${item.img}" alt="${item.title}" height: 240>
-                <p>${item.price}</p>
-                <button class="buy-btn">
-                    <Span class="buy-btn__text">Купить!</Span>
-                </button>
-            </div>`
-};
-
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item));
-    const productsListNew = productsList.join('');
-    document.querySelector('.products').innerHTML = productsListNew;
-};
-
-renderPage(products);*/
