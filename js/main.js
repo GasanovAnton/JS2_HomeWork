d047e1b10dddd71d8406fd09cb6b71b3e31d3c29
GasanovAnton <Kicym1992@gmail.com>
2020-09-03 11:23:47 +0300

    Переделал магазин под Vue
    Переделал магазин под Vue

--- js/main.js
+++ js/main.js
@@ -1,5 +1,94 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
-const btnBasket = document.querySelector(".btn-cart");
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        userSearch: '',
+        showCart: false,
+        catalogUrl: '/catalogData.json',
+        cartUrl: '/getBasket.json',
+        cartItems: [],
+        filtered: [],
+        imgCart: 'https://placehold.it/50x100',
+        products: [],
+        imgProduct: 'https://placehold.it/200x150'
+    },
+    methods: {
+        getJson(url) {
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => console.log(error))
+        },
+        addProduct(item) {
+            this.getJson(`${API}/addToBasket.json`)
+                .then(data => {
+                    if (data.result === 1) {
+                        let find = this.cartItems.find(el => el.id_product === item.id_product);
+                        if (find) {
+                            find.quantity++;
+                        } else {
+                            const prod = Object.assign({ quantity: 1 }, item);
+                            this.cartItems.push(prod)
+                        }
+                    }
+                })
+        },
+        remove(item) {
+            this.getJson(`${API}/addToBasket.json`)
+                .then(data => {
+                    if (data.result === 1) {
+                        if (item.quantity > 1) {
+                            item.quantity--;
+                        } else {
+                            this.cartItems.splice(this.cartItems.indexOf(item), 1);
+                        }
+                    }
+                })
+        },
+        filter(userSearch) {
+            let regexp = new RegExp(userSearch, 'i');
+            this.filtered = this.products.filter(el => regexp.test(el.product_name));
+        }
+    },
+    mounted() {
+        this.getJson(`${API + this.cartUrl}`)
+            .then(data => {
+                for (let item of data.contents) {
+                    this.$data.cartItems.push(item);
+                }
+            });
+        this.getJson(`${API + this.catalogUrl}`)
+            .then(data => {
+                for (let item of data) {
+                    this.$data.products.push(item);
+                    this.$data.filtered.push(item);
+                }
+            });
+        /*this.getJson(`getProducts.json`)
+            .then(data => {
+                for(let item of data){
+                    this.$data.products.push(item);
+                    this.$data.filtered.push(item);
+                }
+            })*/
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const btnBasket = document.querySelector(".btn-cart");
 const basket = document.querySelector(".basket");
 let productInBasketGlobal = [];
 let buttons = [];
@@ -101,7 +190,8 @@ class BasketList {
             if (btn.dataset.id == product.id_product) {
                 product.quantity += 1;
                 product.price += product.price;
-                this.render();
+                this.productInBasket = JSON.stringify(this.productInBasket);
+                let newBasketList = new BasketList();
             }
         })
     }
@@ -158,4 +248,4 @@ btnBasket.addEventListener('click', () => {
 })
 
 
-let newBasketList = new BasketList();
+let newBasketList = new BasketList();*/
